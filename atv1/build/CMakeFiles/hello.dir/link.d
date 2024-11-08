c8cc5bd2af6b7ce4bca80c0ca27b2a8f72aecab6
Paulo Aquino <paulo.aquino@estudante.ufjf.br>
2024-11-07 22:19:54 -0300

    Commit inicial

---
+++ atv1/build/CMakeFiles/hello.dir/link.d
@@ -0,0 +1,19 @@
+hello: \
+  /home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so \
+  /home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so \
+  /home/aquino/miniconda3/envs/cmake-tutorial/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/aquino/miniconda3/envs/cmake-tutorial/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so \
+  /home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so
+
+/home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so:
+
+/home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so:
+
+/home/aquino/miniconda3/envs/cmake-tutorial/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/aquino/miniconda3/envs/cmake-tutorial/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so:
+
+/home/aquino/miniconda3/envs/cmake-tutorial/lib/libgcc_s.so:
---
+++ atv1/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // Imprime a mensagem "Hello, world" no terminal
+    std::cout << "Hello, world" << std::endl;
+
+    return 0;  // Retorna 0 para indicar que o programa terminou com sucesso
+}
